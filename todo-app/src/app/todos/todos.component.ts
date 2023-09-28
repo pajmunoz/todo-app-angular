f2780909cd487b29d2176bd36d09e724c2e99ed4
Pablo Jara <pablojaramunoz@icloud.com>
2023-09-27 21:20:55 -0300

    set dark mode scss - add fonts - add colors

--- todo-app/src/app/todos/todos.component.ts
+++ todo-app/src/app/todos/todos.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent {
+  private class = 'dark';
+  private storage = 'darkMode';
+  state = 'off'
 
+
+  @Input()
+  get value(): boolean {
+    return this.document.body.classList.contains(this.class);
+  }
+
+  set value(isDark: boolean) {
+    localStorage.setItem(this.storage, isDark.toString());
+
+    if (isDark) {
+      this.state='on'
+      this.document.body.classList.add(this.class);
+    } else {
+      this.state='off'
+      this.document.body.classList.remove(this.class);
+    }
+  }
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit() {
+    const value = localStorage.getItem(this.storage);
+    if (value) {
+      this.value = JSON.parse(value);
+    }
+  }
 }
