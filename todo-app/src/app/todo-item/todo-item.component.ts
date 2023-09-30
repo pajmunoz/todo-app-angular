87a0931fa2239b7c8c4bc081415fa2b96c4f0d91
Pablo Jara <pablojaramunoz@icloud.com>
2023-09-30 01:33:36 -0300

    add toggle completed check

--- todo-app/src/app/todo-item/todo-item.component.ts
+++ todo-app/src/app/todo-item/todo-item.component.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 import { Todo } from '../shared/todo.model';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent {
   @Input()
   todo!: Todo;
+  //@Input() todoClicked: EventEmitter<void> = new EventEmitter();
+
+  toggleCompleted(todo:Todo) {
+    todo.completed = !todo.completed
+  }
 }
--- todo-app/src/app/todos/todos.component.ts
+++ todo-app/src/app/todos/todos.component.ts
@@ -64,4 +64,5 @@ export class TodosComponent {
 
     form.reset();
   }
+
 }
